7908e0213ef70b062f9b50409379f88e1ecbf8d0
mv <mvatinel38@gmail.com>
2020-04-02 11:20:52 +0200

    bien avancé

--- Sudoku.py
+++ Sudoku.py
@@ -9,12 +9,29 @@ with open(s1) as f:
 l = 0
 c = 0
 c4 = [0, 1, 2]
-"""Soit on définie une/des valeurs spécifiques pour les
-carrés, Soit on délimite l et c : s2[l:l+2][c:c+2]"""
 
-while l < len(s2):
+#création de la variable qui va vérifier chaque valeurs
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+n = []
+for i in n:
+    i = str(i)
+    n+=i
+
+while l < len(s2): #ça ça fonctionne !
     l = 0
     while c < len(s2[l]):
-        print("ok!")
+        x = 0
+        if c == "|":
+            c+=1
+        elif c == "-":
+            l+=1
+        elif s2[l][c] in n[x]:      #Erreur index or de la liste
+            print("n")
+            #if s2[l][c] == n:
+             #   n-=s2[l][c]
+              #   c+=1
+            #elif s2[l][c] == "_":
+             #   s2[l][c]+=n
         c+=1
     l+=1
+print(s2)

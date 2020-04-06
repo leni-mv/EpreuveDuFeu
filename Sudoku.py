d3e8a078678ac86f5a66dfd16499fab1afc9113d
mv <mvatinel38@gmail.com>
2020-04-06 21:34:15 +0200

    on avance

--- Sudoku.py
+++ Sudoku.py
@@ -1,17 +1,25 @@
+#!/bin/python3.6
+
 import sys
 
 s1 = "/home/leni/Pop/repos/EpreuveDuFeu/s.txt"
 s2 = ""
 with open(s1) as f:
     s2 = f.read().splitlines()
-print(s2)
-print(s2[4])
+x = 0
+for i in s2[x]:
+    if i == "|":
+        print("yes sir")
+        x+=1
+    else:
+        x+=1
+    x+=1
+
+
+
 #assainir les listes de la liste des caractères inutiles.
 #sélectionner caractères spéciaux dans les listes
-for i in s2:
-    s2.remove("|")
 
-print(s2)
 
 
 

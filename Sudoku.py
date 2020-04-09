ebb66544c506791ac4d1f9436f011e1542ae92ec
mv <mvatinel38@gmail.com>
2020-04-09 21:48:00 +0200

    sudoku avance <3

--- Sudoku.py
+++ Sudoku.py
@@ -6,24 +6,66 @@ s1 = "/home/leni/Pop/repos/EpreuveDuFeu/s.txt"
 s2 = ""
 with open(s1) as f:
     s2 = f.read().splitlines()
-x = 0
-for i in s2[x]:
-    if i == "|":
-        print("yes sir")
-        x+=1
-    else:
-        x+=1
-    x+=1
+
+for i in range(len(s2)):
+    s2[i] = s2[i].replace("|", "")      #nettoyage des listes pour une meilleure manipulation par la suite
+    s2[i] = s2[i].replace("_", "0")
+s2.pop(3)
+s2.pop(6)
+
+#definition d'une grille de sudoku vide plus pour int tous les caractères
+#Sinon conversion impossible.
+sudoku=9*[0]
+for i in range(len(sudoku)):
+    sudoku[i]=9*[0]
+
+#convertir les caracteres en entier
+for i in range(0,9):
+    for j in range(0,9):
+        sudoku[i][j] = int(s2[i][j])
+print(sudoku)
 
 
 
-#assainir les listes de la liste des caractères inutiles.
-#sélectionner caractères spéciaux dans les listes
+#Créer fonctions pour vérifier sudoku
+
+def solve_sudoku(sudoku):
+    i = 0
+    while i < len(sudoku):
+        j = 0
+        while j < len(sudoku[i]):
+            if sudoku[i][j] == 0:
+                new_value = 1
+                while new_value < 10:
+                    sudoku[i][j] = new_value
+                    if check_line(sudoku, i) and check_column(sudoku, j) and check_square(sudoku, i, j):
+                        
+                    new_value+=1
+                print(new_value)
+            j+=1
+    
+        i+=1
+
+solve_sudoku(sudoku)
+
+    
+            
 
 
 
 
 
-#Créer 3 fonctions secondaires pour vérifier lignes, colonnes, carrés
+
+
+
+
+
+
+
+
 #Créer une fonction principale pour vérifier les 3 secondaire
 #rajouté les caractères spéciaux et print la liste
+
+#Bien afficher le sudoku
+#for i in range(0,9):
+ #   print(sudoku[i])

411df350d31396d54fcadcb6d283dd72bf04c255
mv <mvatinel38@gmail.com>
2020-04-10 12:40:55 +0200

    on y est presque !

--- Sudoku.py
+++ Sudoku.py
@@ -23,11 +23,57 @@ for i in range(len(sudoku)):
 for i in range(0,9):
     for j in range(0,9):
         sudoku[i][j] = int(s2[i][j])
-print(sudoku)
 
 
 
 #Créer fonctions pour vérifier sudoku
+def init_array():
+    new_array = []
+    val = 1
+    while val < 10:
+        new_array[val] = 0
+        val+=1
+    return(new_array)
+
+def check_line(sudoku, i):
+    array = init_array()
+    j = 0
+    while j < len(sudoku[i]):
+        val = sudoku[i][j]
+        if val > 0 and val < 10 :
+            array[val]+=1
+            if array[val] > 1:
+                return False
+        j+=1
+    return True
+
+def check_column(sudoku, j):
+    array = init_array()
+    i = 0
+    while i < len(sudoku):
+        val = sudoku[i][j]
+        if val > 0 and val < 10 :
+            array[val]+=1
+            if array[val] > 1:
+                return False
+        i+=1
+    return True
+
+def check_square(sudoku, i, j):
+    array = init_array()
+    square_begin_i = (i/4)*4
+    square_begin_j = (j/4)*4
+    i = 0
+    while i < (square_begin_i + 3):
+        while j < (square_begin_j + 3):
+            val = sudoku[i][j]
+            if val > 0 and val < 10 :
+                array[val]+=1
+                if array[val] > 1:
+                    return False
+            j+=1
+        i+=1
+    return True
 
 def solve_sudoku(sudoku):
     i = 0
@@ -38,12 +84,13 @@ def solve_sudoku(sudoku):
                 new_value = 1
                 while new_value < 10:
                     sudoku[i][j] = new_value
-                    if check_line(sudoku, i) and check_column(sudoku, j) and check_square(sudoku, i, j):
-                        
+                    if check_line(sudoku, i) and check_column(sudoku, j) and check_square(sudoku, i, j): #après cette ligne
+                        sudoku = solve_sudoku(sudoku)                                                    #je ne sais pas comment
+                        if sudoku != False:
+                            return sudoku                        
                     new_value+=1
-                print(new_value)
+                return False
             j+=1
-    
         i+=1
 
 solve_sudoku(sudoku)

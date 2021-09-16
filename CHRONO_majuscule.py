518000cfdc8c6c3ba1efba1a75050cb6f67aaeba
Mvatinel <60854444+leni-mv@users.noreply.github.com>
2021-09-16 10:57:25 +0200

    Update CHRONO_majuscule.py

--- CHRONO_majuscule.py
+++ CHRONO_majuscule.py
@@ -1,6 +1,5 @@
 #!/bin/python3.6
-#14h41 #15h16 je recommence tout à cause des " " que j'arrive pas à gérer dans une liste TwT
-#15h32 #51min en regardand ce que j'ai fais pour la gestion des " "
+#Entre 14h41 et 15h32
 
 string = input("Ecris ce que tu veux :")
 longueur = 0
